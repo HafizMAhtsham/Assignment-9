f2bced18463e20d33140591808e66bed38b5b00e
Hafiz Muhammad Ahtsham <118752366+HafizMAhtsham@users.noreply.github.com>
2023-11-21 10:38:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({ storage: storage });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/upload', upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    res.send('File uploaded!');
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
